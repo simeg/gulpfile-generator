af9db14a48dd2590ef26be06ba87ff0aba3a35da
Simon Egersand <s.egersand@gmail.com>
2016-11-07 20:57:26 +0100

    Extract question data to configuration file

--- source/cli.js
+++ source/cli.js
@@ -3,67 +3,28 @@
 
 var inquirer = require('inquirer');
 var generator = require('./generator');
+var questions = require('./cli.config.json').questions;
 
-inquirer.prompt([
-    {
-        type: 'confirm',
-        name: 'devServer',
-        message: 'Do you want a dev server?'
-    },
-    {
-        type: 'checkbox',
-        name: 'jsOptions',
-        message: 'What options do you want for JavaScript?',
-        choices: [
-            {
-                name: 'Concat',
-                short: 'Concat',
-                value: 'concat'
-            },
-            {
-                name: 'Uglify',
-                short: 'Uglify',
-                value: 'uglify'
-            },
-            {
-                name: 'Hint',
-                short: 'Hint',
-                value: 'jshint'
-            },
-            {
-                name: 'Convert ES6 -> ES5',
-                short: 'ES6->ES5',
-                value: 'babel'
-            },
-            {
-                name: 'Compile CoffeeScript',
-                short: 'CoffeeScript',
-                value: 'coffee'
-            }
-        ]
-    },
-    {
-        type: 'input',
-        name: 'jsDistSource',
-        message: 'JS source destination?',
-        default: 'src/scripts',
-        filter: function (val) {
-            if (val.charAt(val.length - 1) === '/')
-                val = val.substring(0, val.length - 1);
-            return val.toLowerCase().trim();
-        }
-    },
-    {
-        type: 'input',
-        name: 'jsDistDest',
-        message: 'JS build destination?',
-        default: 'dist/scripts',
-        filter: function (val) {
-            if (val.charAt(val.length - 1) === '/')
-                val = val.substring(0, val.length - 1);
-            return val.toLowerCase().trim();
-        }
+/*
+ * - Removes trailing slash
+ * - Converts to lower case
+ * - Trims
+ */
+// TODO: Add test for this
+var formatString = function (val) {
+    if (val.charAt(val.length - 1) === '/')
+        val = val.substring(0, val.length - 1);
+    return val.toLowerCase().trim();
+};
+
+var questionsWithFilter = questions.map(function(questionsObj) {
+    if (questionsObj.name === 'jsDistDest' ||
+        questionsObj.name === 'jsDistSource') {
+        questionsObj.filter = formatString;
     }
-]).then(function (answers) {
+    return questionsObj;
+});
+
+inquirer.prompt(questionsWithFilter).then(function(answers) {
     generator.generateFile(answers);
 });
