17b586de541cd45281e94aca3024de1bdbdcc18d
Simon Egersand <s.egersand@gmail.com>
2016-11-14 21:58:02 +0100

    Add tests for CSS options

--- test/generator.spec.js
+++ test/generator.spec.js
@@ -26,6 +26,10 @@ describe('generator', function() {
                 'jsOptions': [],
                 'jsDistSource': 'src/scripts',
                 'jsDistDest': 'dist/scripts',
+                'cssPreProcessorType': 'none',
+                'cssOptions': [],
+                'cssDistSource': 'src/styles',
+                'cssDistDest': 'dist/styles',
                 'outputDependencies': false
             });
         });
@@ -65,6 +69,69 @@ describe('generator', function() {
             checkStringOccurrences(gulpFile, variables);
         };
 
+        var generateFileWithOptions = function(jsOptions, cssOptions) {
+            var config = {
+                'devServer': false,
+                'jsOptions': jsOptions || [],
+                'jsDistSource': 'src/scripts',
+                'jsDistDest': 'dist/scripts',
+                'cssPreProcessorType': 'none',
+                'cssOptions': cssOptions || [],
+                'cssDistSource': 'src/styles',
+                'cssDistDest': 'dist/styles',
+                'outputDependencies': false
+            };
+            generator(config);
+        };
+
+        var runTestUsingSingleIndex = function(type, jsObject, cssObject) {
+            var object = (Object.keys(jsObject).length > 0 ? jsObject : cssObject),
+                option = object.options[object.index],
+                code = object.generatedCode[object.index],
+                result;
+
+            it(option, function() {
+                if (type === 'js') {
+                    generateFileWithOptions([option], null);
+                } else if (type === 'css') {
+                    generateFileWithOptions(null, [option]);
+                }
+                var stateAfter = fs.readFileSync('gulpfile.js', 'utf8');
+
+                if (result = (stateAfter.indexOf(code) === -1))
+                    assert.fail(result, true, 'Code not found');
+
+                assert(true, 'Code for: [' + option + '] found');
+            });
+        };
+
+        // TODO: See if this can be written using runTestUsingSingleIndex()
+        var runTestUsingTwoIndexes = function(type, jsObject, cssObject) {
+            var object = ((jsObject && Object.keys(jsObject).length > 0) ? jsObject : cssObject),
+                firstOption = object.options[object.indexes[0]],
+                firstCode = object.generatedCode[object.indexes[0]],
+                secondOption = object.options[object.indexes[1]],
+                secondCode = object.generatedCode[object.indexes[1]],
+                result;
+
+            it(firstOption + ' and ' + secondOption, function() {
+                if (type === 'js') {
+                    generateFileWithOptions([firstOption, secondOption], null);
+                } else if (type === 'css') {
+                    generateFileWithOptions(null, [firstOption, secondOption]);
+                }
+                var stateAfter = fs.readFileSync('gulpfile.js', 'utf8');
+
+                if (result = (stateAfter.indexOf(firstCode) === -1))
+                    assert.fail(result, true, 'Code not found');
+
+                if (result = (stateAfter.indexOf(secondCode) === -1))
+                    assert.fail(result, true, 'Code not found');
+
+                assert(true, 'Code for: [' + firstOption + ' and ' + secondOption + '] found');
+            });
+        };
+
         describe('with default configuration', function() {
 
             beforeEach(function() {
@@ -85,7 +152,9 @@ describe('generator', function() {
                 var defaultVariables = [
                     "var JS_SOURCE = 'src/scripts'",
                     "var JS_DEST = 'dist/scripts'",
-                    "var JS_OUTPUT_FILE = 'main.js'"
+                    "var JS_OUTPUT_FILE = 'main.js'",
+                    "var CSS_SOURCE = 'src/styles';",
+                    "var CSS_DEST = 'dist/styles';"
                 ];
                 var stateAfter = fs.readFileSync('gulpfile.js', 'utf8');
                 checkVariables(stateAfter, defaultVariables);
@@ -191,66 +260,20 @@ describe('generator', function() {
                 });
             });
 
-            const generateFile = function(jsOptions) {
-                const config = {
-                    'devServer': false,
-                    'jsOptions': jsOptions,
-                    'jsDistSource': 'src/scripts',
-                    'jsDistDest': 'dist/scripts',
-                    'outputDependencies': false
-                };
-                generator(config);
-            };
-
-            const runTestUsingSingleIndex = function(index) {
-                var jsOption = jsOptions[index],
-                    code = generatedCode[index],
-                    result;
-
-                it(jsOption, function() {
-                    generateFile([jsOption]);
-                    var stateAfter = fs.readFileSync('gulpfile.js', 'utf8');
-
-                    if (result = (stateAfter.indexOf(code) === -1))
-                        assert.fail(result, true, 'Code not found');
-
-                    assert(true, 'Code for: [' + jsOption + '] found');
-                });
-            };
-            
-            // TODO: See if this can be written using runTestUsingSingleIndex()
-            const runTestUsingTwoIndexes = function(indexes) {
-                var jsOptionOne = jsOptions[indexes[0]],
-                    codeOne = generatedCode[indexes[0]],
-                    jsOptionTwo = jsOptions[indexes[1]],
-                    codeTwo = generatedCode[indexes[1]],
-                    result;
-
-                it(jsOptionOne + ' and ' + jsOptionTwo, function() {
-                    generateFile([jsOptionOne, jsOptionTwo]);
-                    var stateAfter = fs.readFileSync('gulpfile.js', 'utf8');
-
-                    if (result = (stateAfter.indexOf(codeOne) === -1))
-                        assert.fail(result, true, 'Code not found');
-
-                    if (result = (stateAfter.indexOf(codeTwo) === -1))
-                        assert.fail(result, true, 'Code not found');
-
-                    assert(true, 'Code for: [' + jsOptionOne + ' and ' + jsOptionTwo + '] found');
-                });
-            };
+            var type = 'js';
 
             // These two arrays match against index, so jsOptions[0]
             // generates code that's in generatedCode[0] and so on
-            const jsOptions = [
+            var jsOptions = [
                 "coffee",
                 "jshint",
                 "concat",
                 "babel",
-                "uglify"
+                "uglify",
+                "browserSync"
             ];
 
-            const generatedCode = [
+            var generatedCode = [
                 ".pipe(coffee({bare: true}))",
                 ".pipe(jshint())\n" +
                 "    .pipe(jshint.reporter('default'))",
@@ -258,24 +281,163 @@ describe('generator', function() {
                 ".pipe(babel())",
                 ".pipe(gulp.dest(JS_DEST + '/'))\n" +
                 "    .pipe(rename({suffix: '.min'}))\n" +
-                "    .pipe(uglify())"
+                "    .pipe(uglify())",
+                ".pipe(browserSync.reload({stream:true}))"
             ];
 
             describe('using only one option', function() {
+                var jsObject = {
+                        index: null,
+                        options: jsOptions,
+                        generatedCode: generatedCode
+                    };
                 for (var i = 0; i < jsOptions.length; i++) {
-                    runTestUsingSingleIndex(i);
+                    jsObject.index = i;
+                    runTestUsingSingleIndex(type, jsObject);
                 }
             });
 
             describe('using combination of options', function() {
+                var jsObject = {
+                        indexes: null,
+                        options: jsOptions,
+                        generatedCode: generatedCode
+                    };
                 for (var i = 0; i < jsOptions.length; i++) {
                     for (var j = 0; j < jsOptions.length; j++) {
                         if (j === i)
                             continue;
 
-                        runTestUsingTwoIndexes([i, j]);
+                        jsObject.indexes = [i, j];
+                        runTestUsingTwoIndexes(type, jsObject, null);
                     }
                 }
+            });
+        });
+
+        describe('with CSS option(s)', function() {
+            beforeEach(function() {
+                fsMock({
+                    'gulpfile.js': ''
+                });
+            });
+
+            var type = 'css';
+
+            // These two arrays match against index, so jsOptions[0]
+            // generates code that's in generatedCode[0] and so on
+            var cssOptions = [
+                "autoprefixer",
+                "minifycss",
+                "browserSync"
+            ];
+
+            var generatedCode = [
+                ".pipe(autoprefixer('last 2 versions'))",
+                ".pipe(gulp.dest(CSS_DEST + '/'))\n" +
+                "    .pipe(rename({suffix: '.min'}))\n" +
+                "    .pipe(minifycss())",
+                ".pipe(browserSync.reload({stream:true}))"
+            ];
+
+            describe('using only one option', function() {
+                var cssObject = {
+                        index: null,
+                        options: cssOptions,
+                        generatedCode: generatedCode
+                    };
+                for (var i = 0; i < cssOptions.length; i++) {
+                    cssObject.index = i;
+                    runTestUsingSingleIndex(type, cssObject);
+                }
+            });
+
+            describe('using combination of options', function() {
+                var cssObject = {
+                        indexes: null,
+                        options: cssOptions,
+                        generatedCode: generatedCode
+                    };
+                for (var i = 0; i < cssOptions.length; i++) {
+                    for (var j = 0; j < cssOptions.length; j++) {
+                        if (j === i)
+                            continue;
+
+                        cssObject.indexes = [i, j];
+                        runTestUsingTwoIndexes(type, null, cssObject);
+                    }
+                }
+            });
+
+            describe('with pre processor types', function() {
+                var generateFileWithCssPreProcessorType = function(type) {
+                    var config = {
+                        'devServer': false,
+                        'jsOptions': [],
+                        'jsDistSource': 'src/scripts',
+                        'jsDistDest': 'dist/scripts',
+                        'cssPreProcessorType': type,
+                        'cssOptions': [],
+                        'cssDistSource': 'src/styles',
+                        'cssDistDest': 'dist/styles',
+                        'outputDependencies': false
+                    };
+                    generator(config);
+                };
+
+                var runCssPreProcessorTypeTest = function(type, codeScope) {
+                    var currentFileContent, result;
+                    it(type, function() {
+                        generateFileWithCssPreProcessorType(type);
+
+                        currentFileContent = fs.readFileSync('gulpfile.js', 'utf8');
+
+                        if (result = (currentFileContent.indexOf(codeScope) === -1))
+                            assert.fail(result, true, 'Code not found');
+
+                        assert(true, 'Code for: [' + type + '] found');
+                    });
+                };
+
+                var preProcessorTypes = [
+                    'less',
+                    'sass',
+                    'stylus'
+                ];
+
+                var generatedCode = [
+                    ".pipe(less())",
+                    ".pipe(sass())",
+                    ".pipe(stylus())"
+                ];
+
+                for (var i = 0; i < preProcessorTypes.length; i++) {
+                    var type = preProcessorTypes[i],
+                        codeScope = generatedCode[i];
+                    
+                    runCssPreProcessorTypeTest(type, codeScope);
+                }
+
+                it('none', function() {
+                    var type = 'none',
+                        result = false,
+                        currentFileContent;
+                    generateFileWithCssPreProcessorType(type);
+                    
+                    // Check to see that there's no sign of any pre-processor
+                    for (var i = 0; i < generatedCode.length; i++) {
+                        var codeSnippet = generatedCode[i];
+                        currentFileContent = fs.readFileSync('gulpfile.js', 'utf8');
+                        result = (currentFileContent.indexOf(codeSnippet) === -1);
+                        if (!result) {
+                            assert.fail(result, false, '[' + codeSnippet + '] is in the code, ' +
+                                'it should not be');
+                        }
+                    }
+
+                    assert.ok(true);
+                });
+
             });
         });
     });
@@ -293,6 +455,10 @@ describe('generator', function() {
                 'jsOptions': [],
                 'jsDistSource': 'irrelevantValue',
                 'jsDistDest': 'irrelevantValue',
+                'cssPreProcessorType': 'none',
+                'cssOptions': [],
+                'cssDistSource': 'src/styles',
+                'cssDistDest': 'dist/styles',
                 'outputDependencies': true
             });
             generator(emptyDependenciesConfig);
@@ -307,6 +473,10 @@ describe('generator', function() {
                 'jsOptions': ['uglify', 'babel', 'jshint'],
                 'jsDistSource': 'irrelevantValue',
                 'jsDistDest': 'irrelevantValue',
+                'cssPreProcessorType': 'none',
+                'cssOptions': [],
+                'cssDistSource': 'src/styles',
+                'cssDistDest': 'dist/styles',
                 'outputDependencies': true
             });
             generator(installDependenciesConfig);
