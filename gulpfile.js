9b918dbb8e441c2bec980e61e7732b49fd5a4b21
Simon Egersand <s.egersand@gmail.com>
2016-11-20 22:25:21 +0100

    Sort Gulp tasks in executing order

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,9 @@ var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
 var plumber = require('gulp-plumber');
 
-gulp.task('static', function() {
-    gulp.src('source/*.js')
-        .pipe(excludeGitignore())
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+gulp.task('set-test-env', function(cb) {
+    process.env.NODE_ENV = 'test';
+    cb();
 });
 
 gulp.task('json', ['set-test-env'], function(cb) {
@@ -21,6 +18,14 @@ gulp.task('json', ['set-test-env'], function(cb) {
         .pipe(jsonlint())
         .pipe(jsonlint.reporter());
     cb();
+});
+
+gulp.task('static', function() {
+    gulp.src('source/*.js')
+        .pipe(excludeGitignore())
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('test', ['set-test-env'], function(cb) {
@@ -36,11 +41,6 @@ gulp.task('test', ['set-test-env'], function(cb) {
         .on('end', function() {
             cb(mochaErr);
         });
-});
-
-gulp.task('set-test-env', function(cb) {
-    process.env.NODE_ENV = 'test';
-    cb();
 });
 
 gulp.task('default', ['set-test-env', 'json', 'static', 'test']);
